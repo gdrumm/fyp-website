c0a00ae1b1a5256a81b8b70980d9cfc6d31d175d
Gavin <gavin.drumm2@mail.dcu.ie>
2017-03-06 15:17:51 +0000

    First commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  get 'pages/home'
+
+  resources :listings
+  get 'pages/about'
+
+  get 'pages/contact'
+
+  root 'listings#index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
